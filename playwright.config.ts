b0c28eb106e6b62e37ff4dbc542f0e6dca63abdc
Siddheshwar Anajekar <siddheshwar.anajekar@aurionpro.com>
2025-11-28 11:54:57 +0530

    new report added

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,39 @@
 import { defineConfig, devices } from '@playwright/test';
+import { OrtoniReportConfig } from "ortoni-report";
+import * as os from "os";
+const reportConfig: OrtoniReportConfig = {
+  open: process.env.CI ? "never" : "always",
+  folderPath: "my-report",
+  filename: "Ally Portal.html",
+  title: "Ally Portal UI Test Report",
+  showProject: false,
+  projectName: "Ally Portal",
+  testType: "E2E-Functional",
+  authorName: os.userInfo().username,
+  base64Image: false,
+
+  // Branding
+ // logo: "AndDone.png",
+  headerText: "Ally Portal UI Automation Report",  // custom property
+  //customCss: "custom.css",
+logo: "./assets/AllyLogoDark.svg",
+
+
+  // Enables clickable dashboard
+  stdIO: true,
+
+  meta: {
+    "Test Cycle": "AN_ALMGMT_V12",
+    Environment: process.env.NODE_ENV || "Local",
+    version: "1",
+    release: "V12",
+    platform: os.type(),
+  },
+} as any;  // <-- bypass TypeScript type checking for headerText
+
+
+  
+
 
 export default defineConfig({
   testDir: './tests',
@@ -6,11 +41,15 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  // Required for Ortoni to read test artifacts
+  outputDir: "test-results",
 
   //  Existing HTML report + Allure added
   reporter: [
-    ['html'],                   // existing
-    ['allure-playwright']       // ⭐ Allure report adapter
+     ["ortoni-report", reportConfig],
+    // ['html']   
+                    // existing
+        
   ],
 
   use: {
